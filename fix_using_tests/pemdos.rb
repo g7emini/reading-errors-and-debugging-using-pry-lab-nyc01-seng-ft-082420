bbebcd5c6c6542ceb6cc9bfcd33b5cb4385d4790
groana <contact@groanamelendez.com>
2020-08-02 23:41:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,11 +1,9 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
     # 10 * "s" + string #original code
-    puts 10.times "s" + string #my code not working
-    binding.pry
+    
   else
     string
   end
